3e18c128212b6e76a461a027b75c0b3414d670cb
slice <ryaneft@gmail.com>
2019-04-19 17:02:04 -0700

    Add more cases for the missing_cog_listener test

--- clyde/test/input/missing_cog_listener.py
+++ clyde/test/input/missing_cog_listener.py
@@ -4,23 +4,28 @@ from discord.ext import commands
 from discord.ext.commands import Cog
 
 
-class A(commands.Cog):
+class Bad1(commands.Cog):
     async def on_message(self, _message):  # [missing-cog-listener]
         pass
 
 
-class B(Cog):
+class Bad2(Cog):
     async def on_message(self, _message):  # [missing-cog-listener]
         pass
 
 
-class C(Cog):
+class Good1(Cog):
     @commands.Cog.listener()
     async def on_message(self, _message):
         pass
 
 
-class D(Cog):
+class Good2(Cog):
     @Cog.listener()
     async def on_message(self, _message):
+        pass
+
+
+class Good3(Cog):
+    async def on_something_happening(self):
         pass
