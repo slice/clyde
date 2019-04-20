1719987f269124a966befda58041f4b9a6da2c66
slice <ryaneft@gmail.com>
2019-04-19 17:26:41 -0700

    Add pylint and astroid to install_requires

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setuptools.setup(
     version='0.0.0',
     author='slice',
     description='A Pylint plugin for Discord.py',
+    install_requires=[
+        'pylint',
+        'astroid'
+    ],
     packages=setuptools.find_packages()
 )
